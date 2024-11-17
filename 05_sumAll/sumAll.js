2f9a86c993a3cc763f038ed46633b54850c16648
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-17 16:39:11 -0600

    Added check for negative values. All tests passed. Exercise 05 complete.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,11 @@ const sumAll = function(firstInt, secondInt) {
   if(!(Number.isInteger(firstInt) && Number.isInteger(secondInt))) {
     return "ERROR";
   }
+
+  if (firstInt < 0 || secondInt < 0) {
+    return "ERROR";
+  }
+
   const lowerInt = Math.min(firstInt, secondInt);
   const upperInt = Math.max(firstInt, secondInt);
   let result = 0;
