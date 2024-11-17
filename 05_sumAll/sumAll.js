e96ce56734156b9c0beac9c0a24e5df007d3c631
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-17 16:35:51 -0600

    First draft for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+  const lowerInt = Math.min(firstInt, secondInt);
+  const upperInt = Math.max(firstInt, secondInt);
+  let result = 0;
+  for (let i = lowerInt; i <= upperInt; i++) {
+    result += i;
+  }
 
+  return result;
 };
 
 // Do not edit below this line
