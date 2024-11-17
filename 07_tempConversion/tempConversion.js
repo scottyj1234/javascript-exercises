7d61a76e7fe9e02cc1eb78a115811f0bc6d4dd98
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-17 16:47:02 -0600

    First draft for convertToCelsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+
+  if (!isFinite(degreesFahrenheit)) {
+    return "ERROR";
+  }
+
+  const fullResult = (degreesFahrenheit - 32) * 5 / 9;
+
+  // round to 1 decimal
+  return Math.round(fullResult * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
 };
 
 // Do not edit below this line
