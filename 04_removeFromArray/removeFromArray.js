5a6e7e73578067c58413d1fb44a2e975148f8b5d
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-17 16:32:12 -0600

    First draft of exercise 04. Passed 1st test.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(fullArray, ...elementsToRemove) {
+  let resultArray = fullArray;
+  for(let i = 0; i < elementsToRemove.length; i++) {
+    const elementToRemove = elementsToRemove[i];
+    let indexOfElement = resultArray.indexOf(elementToRemove);
+    while (indexOfElement != -1) {
+      resultArray.splice(indexOfElement, 1);
+      indexOfElement = resultArray.indexOf(elementToRemove);
+    }
+  }
+
+  return resultArray;
 };
 
 // Do not edit below this line
