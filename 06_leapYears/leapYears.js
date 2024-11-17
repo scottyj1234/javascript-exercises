32864af984c1936d7074517f5580790c2c5887be
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-17 16:42:57 -0600

    First draft of leapyears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (!Number.isInteger(year)) {
+    return "ERROR";
+  }
+  
+  return (year % 4 === 0 && (!(year % 100 === 0) || year % 400 === 0));
 };
 
 // Do not edit below this line
