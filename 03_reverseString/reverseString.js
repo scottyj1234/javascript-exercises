b7ead6d7f54114b216a60992d0c97405a59bfa00
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-17 16:16:30 -0600

    Exercise 03 Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(forwardString) {
+  const stringLength = forwardString.length;
+  let result = "";
+  for (let i = stringLength - 1; i >= 0; i--) {
+    result += forwardString[i];
+  }
 
+  return result;
 };
 
 // Do not edit below this line
