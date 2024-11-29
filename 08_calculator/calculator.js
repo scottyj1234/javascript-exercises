590b7a34e3c1e1115816633863096c03da3243a1
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-29 16:01:49 -0600

    First draft of calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, val) => acc + val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, val) => acc * val, 0)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+	if (val === 1) return 1;
+  return val * factorial(val - 1);
 };
 
 // Do not edit below this line
