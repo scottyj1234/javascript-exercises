d1dfcaa8b308c7f460ccb8f8e036e30bc97a0d70
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-29 16:18:44 -0600

    First draft of palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const strLower = str.toLowerCase();
+  const strArray = strLower.split('');
+  const strArrayOnlyCharacters = strArray.filter((char) => {
+    return char >= "a" || char <= "z"
+  });
 
+  for (let i = 0; i <= strArrayOnlyCharacters.length; ++i) {
+    const frontLetter = strArrayOnlyCharacters[i];
+    const backLetter = strArrayOnlyCharacters[-1 * (i + 1)];
+    if (frontLetter !== backLetter) {
+      return false;
+    }
+    return true;
+  }
 };
 
 // Do not edit below this line
