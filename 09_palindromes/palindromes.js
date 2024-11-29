f5523b5bb418fea3f585bd3f963bbe8adaf4c9f8
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-29 16:33:00 -0600

    Bugfix: Fix filtering to include letters and numbers. All tests pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,18 +2,19 @@ const palindromes = function (str) {
   const strLower = str.toLowerCase();
   const strArray = strLower.split('');
   const strArrayOnlyCharacters = strArray.filter((char) => {
-    return char >= "a" || char <= "z"
+    return (char.localeCompare("a") >= 0 && char.localeCompare("z") <= 0) || (char.localeCompare("0") >= 0 && char.localeCompare("9") <= 0)
   });
 
-  for (let i = 0; i <= strArrayOnlyCharacters.length; ++i) {
+  for (let i = 0; i <= strArrayOnlyCharacters.length / 2; ++i) {
     const frontLetter = strArrayOnlyCharacters.at(i);
     const backLetter = strArrayOnlyCharacters.at(-1 * (i + 1));
     console.log(`${frontLetter}; ${backLetter}`);
     if (frontLetter !== backLetter) {
       return false;
     }
-    return true;
   }
+
+  return true;
 };
 
 // Do not edit below this line
