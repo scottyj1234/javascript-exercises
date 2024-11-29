46e6368140ecafee0416ac48efd11dec54901539
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-29 16:38:23 -0600

    Fix for the case of 0. Change for only specific case of 1.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,11 +1,15 @@
 const fibonacci = function(fibIndex) {
-  if (fibIndex <= 2) {
+  if (fibIndex <= 0) {
+    return 0;
+  }
+
+  if (fibIndex === 1) {
     return 1;
   }
 
-  let num1 = 1;
+  let num1 = 0;
   let num2 = 1;
-  for (i = 0; i < (fibIndex - 2); ++i) {
+  for (i = 0; i < (fibIndex - 1); ++i) {
     const nextNum = num1 + num2;
     num1 = num2;
     num2 = nextNum;
