c7888a066075dd80378dcae24f396cc6b5202694
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-29 16:53:31 -0600

    First draft of findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  peopleAgeMap = people.map((person) => {
+    let age = 0;
 
+    if (person.yearOfDeath === undefined) {
+      age = Date().getFullYear() - person.yearOfBirth;
+    } else {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
+
+    return {
+      person,
+      age
+    }
+  });
+
+  return peopleAgeMap.reduce((acc, val) => {
+    if (val.age > acc.age) return val;
+    return acc;
+  }).person
 };
 
 // Do not edit below this line
