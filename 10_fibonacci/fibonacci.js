f10ca6e657fc0ca1023c2556c1f7e0543b9b0ee3
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-29 16:36:27 -0600

    First draft of fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(fibIndex) {
+  if (fibIndex <= 2) {
+    return 1;
+  }
 
+  let num1 = 1;
+  let num2 = 1;
+  for (i = 0; i < (fibIndex - 2); ++i) {
+    const nextNum = num1 + num2;
+    num1 = num2;
+    num2 = nextNum;
+  }
+
+  return num2;
 };
 
 // Do not edit below this line
